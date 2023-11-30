9bba0967b94c8bdaa90f84506c6bdd72b567a1d6
Yuehan-Wang <yuehan@uchicago.edu>
2023-11-30 09:54:49 -0600

    First commit

---
+++ src/component/Header.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import styles from 'src/styles/Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+        <h1 className={styles.title}>
+            <Link href="/" passHref><span>Yuehan Blog</span></Link>
+        </h1>
+      <nav>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link href="/" passHref><span>Home</span></Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/project" passHref><span>Project</span></Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/contact" passHref><span>Contact</span></Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/post" passHref><span>Post</span></Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
